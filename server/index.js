6379adb8a4b8eb9d18739293ed110c5ab9074e3c
Bohdan Haponiuk <jlomka54@gmail.com>
2026-06-30 11:46:47 +0400

    connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import dns from "dns";
 import mongoose from "mongoose";
 
 dotenv.config();
 
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 const PORT = process.env.PORT || 3001;
+const mongoUri =
+  process.env.MONGO_DB_URL ??
+  `mongodb+srv://${encodeURIComponent(process.env.MONGO_DB_USERNAME)}:${encodeURIComponent(process.env.MONGO_DB_PASSWORD)}@cluster0.tuy6u.mongodb.net/${encodeURIComponent(process.env.MONGO_DB_NAME)}?retryWrites=true&w=majority`;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL);
+    await mongoose.connect(mongoUri);
     console.log("Connected to MongoDB");
 
     app.listen(PORT, () => {
