8ad2d2c6d39608366df1cf7f5dc98b5f2b7ab1ab
Bohdan Haponiuk <jlomka54@gmail.com>
2026-07-02 14:31:51 +0400

    feat: add protected routes

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -5,19 +5,32 @@ import { NotFoundPage } from "../pages/NotFoundPage/NotFoundPage";
 import MainLayout from "../layouts/MainLayout/MainLayout";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
+import ProtectedRoute from "./ProtectedRoute";
+import { useSelector } from "react-redux";
+import { selectIsAuthenticated } from "../features/auth/authSelectors";
+
+const RootRedirect = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  return (
+    <Navigate to={isAuthenticated ? "/orders" : "/login"} replace />
+  );
+};
 
 const AppRouter = () => {
   return (
     <div>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Navigate to="/orders" replace />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/orders" element={<OrdersPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<RootRedirect />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path="/orders" element={<OrdersPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+          </Route>
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
