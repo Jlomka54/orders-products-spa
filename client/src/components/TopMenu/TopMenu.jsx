dfac08b4643b73e7948df9c75455e281f3b2fd64
Bohdan Haponiuk <jlomka54@gmail.com>
2026-07-02 17:15:50 +0400

    feat: add realtime clock to top menu

--- client/src/components/TopMenu/TopMenu.jsx
+++ client/src/components/TopMenu/TopMenu.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const formatWeekday = (date) =>
+  date.toLocaleDateString("en-US", { weekday: "long" });
+
+const formatDate = (date) =>
+  date.toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+
+const formatTime = (date) =>
+  date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
 const TopMenu = () => {
+  const activeSessions = useSelector((state) => state.ui.activeSessions);
+  const [currentDate, setCurrentDate] = useState(() => new Date());
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <ul>
-      <li>Today</li>
-      <li>12 MAR, 2017</li>
-      <li>17:20</li>
-      <li>Sessions: 1</li>
+      <li>{formatWeekday(currentDate)}</li>
+      <li>{formatDate(currentDate)}</li>
+      <li>{formatTime(currentDate)}</li>
+      <li>Sessions: {activeSessions}</li>
     </ul>
   );
 };
