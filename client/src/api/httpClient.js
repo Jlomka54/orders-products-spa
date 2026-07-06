574d24e06682bbb9e1e318d4599508c5f0f5e8e1
Bohdan Haponiuk <jlomka54@gmail.com>
2026-07-06 16:02:06 +0400

    url

--- client/src/api/httpClient.js
+++ client/src/api/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -21,9 +21,7 @@ httpClient.interceptors.response.use(
   (response) => response,
   (error) => {
     const message =
-      error.response?.data?.message ||
-      error.message ||
-      "Something went wrong";
+      error.response?.data?.message || error.message || "Something went wrong";
 
     return Promise.reject(new Error(message));
   },
