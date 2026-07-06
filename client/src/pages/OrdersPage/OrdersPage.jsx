5df34beb59c44a23c5de0a6d93ef2a1342e8d228
Bohdan Haponiuk <jlomka54@gmail.com>
2026-07-06 10:54:33 +0400

    Create arrivals list components

--- client/src/pages/OrdersPage/OrdersPage.jsx
+++ client/src/pages/OrdersPage/OrdersPage.jsx
@@ -3,41 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import EmptyState from "../../components/ui/EmptyState";
 import ErrorMessage from "../../components/ui/ErrorMessage";
 import Loader from "../../components/ui/Loader";
+import ArrivalsList from "../../features/arrivals/components/ArrivalsList";
 import DeleteOrderModal from "../../features/orders/components/DeleteOrderModal";
-import OrderDetailsPanel from "../../features/orders/components/OrderDetailsPanel";
-import OrderFormModal from "../../features/orders/components/OrderFormModal";
-import OrdersList from "../../features/orders/components/OrdersList";
 import {
   selectDeleteModalOrder,
   selectDeleteModalOrderId,
-  selectEditingOrder,
-  selectOrderFormMode,
-  selectOrderFormOpen,
   selectOrders,
   selectOrdersError,
   selectOrdersLoading,
-  selectOrdersMutationLoading,
-  selectSelectedOrderId,
-  selectSelectedOrderDetails,
 } from "../../features/orders/ordersSelectors";
 import {
   closeDeleteModal,
-  closeOrderFormModal,
-  createOrder,
-  fetchOrderById,
   fetchOrders,
-  openCreateOrderModal,
   openDeleteModal,
-  openEditOrderModal,
   removeOrder,
-  setSelectedOrderId,
-  updateOrder,
 } from "../../features/orders/ordersSlice";
-import {
-  getOrderId,
-  getOrderRequestId,
-  isSameOrder,
-} from "../../utils/orderHelpers";
 import "./OrdersPage.css";
 
 const OrdersPage = () => {
@@ -45,20 +25,9 @@ const OrdersPage = () => {
   const orders = useSelector(selectOrders);
   const isLoading = useSelector(selectOrdersLoading);
   const error = useSelector(selectOrdersError);
-  const selectedOrderId = useSelector(selectSelectedOrderId);
-  const selectedOrderDetails = useSelector(selectSelectedOrderDetails);
   const deleteModalOrderId = useSelector(selectDeleteModalOrderId);
   const deleteModalOrder = useSelector(selectDeleteModalOrder);
-  const isOrderFormOpen = useSelector(selectOrderFormOpen);
-  const orderFormMode = useSelector(selectOrderFormMode);
-  const editingOrder = useSelector(selectEditingOrder);
-  const mutationLoading = useSelector(selectOrdersMutationLoading);
 
-  const selectedDetailsId = getOrderId(selectedOrderDetails);
-  const activeSelectedOrderDetails =
-    isSameOrder(selectedDetailsId, selectedOrderId)
-      ? selectedOrderDetails
-      : null;
   const hasOrders = orders.length > 0;
   const isInitialLoading = isLoading && !hasOrders;
   const hasBlockingError = error && !hasOrders;
@@ -66,36 +35,6 @@ const OrdersPage = () => {
   useEffect(() => {
     dispatch(fetchOrders());
   }, [dispatch]);
-
-  const handleOrderClick = (orderId) => {
-    dispatch(setSelectedOrderId(orderId));
-    dispatch(fetchOrderById(orderId));
-  };
-
-  const handleOpenCreateOrderModal = () => {
-    dispatch(openCreateOrderModal());
-  };
-
-  const handleOpenEditOrderModal = (order) => {
-    dispatch(openEditOrderModal(order));
-  };
-
-  const handleCloseOrderFormModal = () => {
-    dispatch(closeOrderFormModal());
-  };
-
-  const handleOrderSubmit = (payload) => {
-    if (orderFormMode === "create") {
-      dispatch(createOrder(payload));
-      return;
-    }
-
-    const orderId = getOrderRequestId(editingOrder);
-
-    if (orderId !== null && orderId !== undefined) {
-      dispatch(updateOrder({ orderId, orderPayload: payload }));
-    }
-  };
 
   const handleOpenDeleteModal = (orderId) => {
     dispatch(openDeleteModal(orderId));
@@ -124,15 +63,7 @@ const OrdersPage = () => {
   return (
     <section className="orders-page">
       <header className="orders-page__header">
-        <button
-          className="orders-page__create-button"
-          type="button"
-          aria-label="Add order"
-          onClick={handleOpenCreateOrderModal}
-        >
-          + Add order
-        </button>
-        <h1 className="orders-page__heading">Orders / {orders.length}</h1>
+        <h1 className="orders-page__heading">Приходы / {orders.length}</h1>
       </header>
 
       {(error || isLoading) && (
@@ -148,32 +79,13 @@ const OrdersPage = () => {
       )}
 
       {hasOrders ? (
-        <div className="orders-page__content">
-          <OrdersList
-            orders={orders}
-            selectedOrderId={selectedOrderId}
-            onOrderSelect={handleOrderClick}
-            onDeleteOrder={handleOpenDeleteModal}
-            onEditOrder={handleOpenEditOrderModal}
-          />
-
-          <OrderDetailsPanel
-            selectedOrderDetails={activeSelectedOrderDetails}
-            onEditOrder={handleOpenEditOrderModal}
-          />
-        </div>
+        <ArrivalsList
+          orders={orders}
+          onDeleteOrder={handleOpenDeleteModal}
+        />
       ) : (
         <EmptyState message="No orders found." />
       )}
-
-      <OrderFormModal
-        isOpen={isOrderFormOpen}
-        mode={orderFormMode}
-        order={editingOrder}
-        isLoading={mutationLoading}
-        onClose={handleCloseOrderFormModal}
-        onSubmit={handleOrderSubmit}
-      />
 
       <DeleteOrderModal
         deleteModalOrderId={deleteModalOrderId}
