0c4429f74fa2cab214ea9608b71c5f4df1c23110
Bohdan Haponiuk <jlomka54@gmail.com>
2026-07-06 10:00:09 +0400

    Add order form selectors

--- client/src/features/orders/ordersSelectors.js
+++ client/src/features/orders/ordersSelectors.js
@@ -9,10 +9,19 @@ export const selectOrdersLoading = (state) => state.orders.isLoading;
 
 export const selectOrdersError = (state) => state.orders.error;
 
+export const selectOrdersMutationLoading = (state) =>
+  state.orders.mutationLoading;
+
 export const selectSelectedOrderId = (state) => state.orders.selectedOrderId;
 
 export const selectSelectedOrderDetails = (state) =>
   state.orders.selectedOrderDetails;
+
+export const selectOrderFormOpen = (state) => state.orders.isOrderFormOpen;
+
+export const selectOrderFormMode = (state) => state.orders.orderFormMode;
+
+export const selectEditingOrder = (state) => state.orders.editingOrder;
 
 export const selectDeleteModalOrderId = (state) =>
   state.orders.deleteModalOrderId;
